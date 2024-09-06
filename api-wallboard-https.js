0d045cc68a764e193d51576307769745178a4fa3
Missez <lertpisi@gmail.com>
2024-09-06 13:32:37 +0700

    ip

--- api-wallboard-https.js
+++ api-wallboard-https.js
@@ -13,7 +13,7 @@ const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 var apiport = 8305;
 
 const config = {
-  databaseURI: 'mongodb://team3_web_labDB:WB1qazxsw2@10.21.43.203:27017/team3_wallboarddb',
+  databaseURI: 'mongodb://team3_web_labDB:WB1qazxsw2@127.0.0.1:27017/team3_wallboarddb',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'wallboardapi',
   masterKey: process.env.MASTER_KEY || 'wallboardapi', //Add your master key here. Keep it secret!
